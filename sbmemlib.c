a85809168baacd207ba1661cf2fe00dcc85fa10e
Osman Batur Ince <osmanbaturince@outlook.com>
2021-04-11 19:00:09 +0300

    done somethings

--- sbmemlib.c
+++ sbmemlib.c
@@ -20,9 +20,13 @@ struct Node {
     pid_t pid;
 };
 
-int createdBefore = 0;
+struct Process {
+    pid_t pid;
+    struct Process* next;
+};
 
-struct Node* head;
+struct Node* head = NULL;
+struct Process* phead = NULL;
 
 int sbmem_init(int segmentsize)
 {
@@ -30,8 +34,7 @@ int sbmem_init(int segmentsize)
 
     printf ("sbmem init called"); // remove all printfs when you are submitting to us.
 
-    if(createdBefore)
-        sbmem_remove();
+    /* DIZLA */
 
     fd = shm_open(SHM_NAME, O_CREAT, 0666);
 
@@ -43,7 +46,16 @@ int sbmem_init(int segmentsize)
 
     createdBefore = 1;
 
-    head = malloc(sizeof(struct Node));
+    void *shared_mem = mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if( shared_mem == MAP_FAILED){
+        perror("mmap err");
+    }
+    head = sbmem_alloc(sizeof(struct Node));
+
+    head->size = -1;
+    head->next = NULL;
+    head->mem_ptr = NULL;
+    head->pid = -1;
 
     return 0;
 }
@@ -52,19 +64,60 @@ int sbmem_remove()
 {
     shm_unlink(SHM_NAME);
     sem_unlink(SEM_NAME);
+    /*while( head){
+        struct Node temp = head->next;
+        head->next = NULL;
+        free(head);
+        head = temp;
+    }*/
 
     return (0);
 }
 
 int sbmem_open()
 {
+    int fd = shm_open(SHM_NAME, O_RDWR, /*0600*/0666);
+    void *shared_mem = mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if( shared_mem == MAP_FAILED){
+        perror("mmap err");
+    }
+    struct Process* cur;
+    int count = 0;
+    for( cur = phead; cur != NULL; cur = cur->next){
+        count++;
+        if(cur->pid == getpid()){
+            return 0;
+        }
+    }
+
+    if( count == 10){
+        return -1;
+    }
+
+    struct Process* pnext = sbmem_alloc(sizeof(struct Process));
+    pnext->pid = getpid();
+    pnext->next = NULL;
+    insertProcess(pnext);
 
     return (0);
 }
 
+void insertProcess(struct Process* pnext){
+    if(!phead){
+        phead = pnext;
+    } else {
+        struct Process* cur = phead;
+        while(!(cur->next)){
+            cur = cur->next;
+        }
+        cur->next = pnext;
+    }
+}
 
-void *sbmem_alloc (int size)
+
+void *sbmem_alloc(int size)
 {
+    if( head == NULL)
     return (NULL);
 }
 
